48e5353341d96bf1643d79c6407f7a0baa5ce952
Kapas Zsombor <kapas.zsombor@gmail.com>
2020-05-22 11:21:23 +0200

    Uploading Module4 Assingment solution

---
+++ module4/harder/SpeakGoodBye.js
@@ -0,0 +1,12 @@
+(function (window) {
+    
+    var byeSpeaker = {}
+    byeSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+    }
+    
+    window.byeSpeaker = byeSpeaker;
+    
+    var speakWord = "Goodbye";
+    
+})(window);
